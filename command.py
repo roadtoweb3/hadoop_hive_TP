9c76924c53e65c72be919fbe0c873a050aa79681
roadtoweb3 <95033731+roadtoweb3@users.noreply.github.com>
2021-11-30 16:55:15 +0100

    Create command.py

---
+++ command.py
@@ -0,0 +1,14 @@
+echo '
+
+# HADOOP PATHS
+export JAVA_HOME=/usr
+export PATH=$PATH:/usr/bin/java
+export HADOOP_HOME=/home/ubuntu/hadoop
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export PATH=$PATH:$HADOOP_HOME/bin
+
+' >> ~/.bashrc
