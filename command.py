642e0b16a78783a745bd0bb87ab7739a6aac737a
roadtoweb3 <95033731+roadtoweb3@users.noreply.github.com>
2021-12-01 12:31:03 +0100

    Update command.py

--- command.py
+++ command.py
@@ -12,3 +12,32 @@ export YARN_HOME=$HADOOP_HOME
 export PATH=$PATH:$HADOOP_HOME/bin
 
 ' >> ~/.bashrc
+
+<property>
+<name>dfs.replication</name>
+<value>1</value>
+</property>
+<property>
+<name>dfs.permission</name>
+<value>false</value>
+</property>
+<property>
+<name>dfs.name.dir</name>
+<value>/home/ubuntu/data/data-namenode</value>
+</property>
+<property>
+<name>dfs.namenode.http-address</name>
+<value>0.0.0.0:50070</value>
+</property>
+<property>
+<name>dfs.data.dir</name>
+<value>/home/ubuntu/data/data-datanode1</value>
+</property>
+<property>
+<name>dfs.datanode.address</name>
+<value>0.0.0.0:50010</value>
+</property>
+<property>
+<name>dfs.datanode.http.address</name>
+<value>0.0.0.0:50075</value>
+</property>
